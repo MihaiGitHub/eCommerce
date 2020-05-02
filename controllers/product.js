4ebe15e92619e928bce91b0d91bceba5881410a6
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-05-01 22:23:43 -0700

    Product form fields validation

--- controllers/product.js
+++ controllers/product.js
@@ -22,6 +22,15 @@ exports.create = (req, res) => {
             });
         }
 
+        // check for all fields
+        const { name, description, price, category, quantity, shipping } = fields;
+
+        if(!name || !description || !price || !category || !quantity || !shipping){
+            return res.status(400).json({
+                error: 'All fields are required!'
+            });
+        }
+
         let product = new Product(fields);
 
         if(files.photo){
