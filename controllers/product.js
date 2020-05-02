7b31eadb948d04a574085e3c6d6bcd21482364d9
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-05-01 19:12:22 -0700

    File upload max size

--- controllers/product.js
+++ controllers/product.js
@@ -25,6 +25,12 @@ exports.create = (req, res) => {
         let product = new Product(fields);
 
         if(files.photo){
+            if(files.photo.size > 1000000){
+                return res.status(400).json({
+                    error: 'Image should be less than 1mb in size'
+                });
+            }
+
             product.photo.data = fs.readFileSync(files.photo.path);
             product.photo.contentType = files.photo.type;
         }
