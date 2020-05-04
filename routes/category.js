db4cd804683ca105349c31e1698e96c056d2e6c3
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-05-03 17:55:29 -0700

    Create category update, remove and list methods

--- routes/category.js
+++ routes/category.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-const { create } = require('../controllers/category');
+const { create, categoryById, read, update, remove, list } = require('../controllers/category');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
 // middleware
 // anytime there is a userId param in the route execute userById method
 router.param('userId', userById);
+router.param('categoryId', categoryById);
 
 // routes
 // use requireSignin, isAuth, isAdmin as middleware for this route
 router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
+router.put('/category/:categoryId/:userId', requireSignin, isAuth, isAdmin, update);
+router.delete('/category/:categoryId/:userId', requireSignin, isAuth, isAdmin, remove);
+router.get('/category/:categoryId', read);
+router.get('/categories', list);
 
 module.exports = router;
