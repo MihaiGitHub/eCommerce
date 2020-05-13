4e8afe58d2ea4f2389d1a95452caa7acf140b294
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-05-12 22:16:13 -0700

    Get product list method

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, productById, read, remove, update } = require('../controllers/product');
+const { create, productById, read, remove, update, list } = require('../controllers/product');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
@@ -16,5 +16,6 @@ router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
 router.get('/product/:productId', read);
 router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove);
 router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update);
+router.get('/products', list);
 
 module.exports = router;
