93519e90c5399c8673e588bee719d8b4dfb6eb34
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-06-14 17:35:17 -0700

    Create AddProduct component

--- client/src/admin/apiAdmin.js
+++ client/src/admin/apiAdmin.js
@@ -18,3 +18,21 @@ export const createCategory = (userId, token, category) => {
         console.log(err);
     })
 }
+
+export const createProduct = (userId, token, product) => {
+    // returns a promise; make promise available using return fetch so can use .then
+    return fetch(`${API}/product/create/${userId}`, {
+        method: 'POST',
+        headers: { // backend will respond with json data so need to accept it
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`
+        }, // send as form data
+        body: product
+    })
+    .then(response => {
+        return response.json();
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
