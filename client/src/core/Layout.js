32c73097af91ebaa3e1b114a9a5209dd159d592e
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-07-07 17:01:10 -0700

    Create app stylesheet

--- client/src/core/Layout.js
+++ client/src/core/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Menu from './Menu';
+import "../styles.css";
 
 const Layout = ({ title = 'Title', description = 'Description', className, children }) => (
     <div>
         <Menu />
-        <div className="jumbotron">Home
+        <div className="jumbotron">
             <h2>{title}</h2>
             <p className="lead">{description}</p>
         </div>
