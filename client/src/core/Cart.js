a7075adaddd3c3f52e0ca59636f0dd67a905dfbc
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-09-05 21:34:03 -0700

    Shopping cart total

--- client/src/core/Cart.js
+++ client/src/core/Cart.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import Layout from './Layout';
 import { getCart } from './cartHelpers';
 import Card from './Card';
+import Checkout from './Checkout';
 
 const Cart = () => {
     const [run, setRun] = useState(false);
@@ -50,7 +51,9 @@ const Cart = () => {
                     </div>
 
                     <div className="col-6">
-                        <p>show checkout options/shipping address/total/update quantity</p>
+                        <h2 className="mb-4">Your cart summary</h2>
+                        <hr />
+                        <Checkout products={items} />
                     </div>
                 </div>
         </Layout>
