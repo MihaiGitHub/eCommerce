97ffa3ac6999eb42bd96bc3c51d93cfb7009f8b5
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-10-30 17:24:41 -0700

    Route handler for processing payment

--- api/routes/braintree.js
+++ api/routes/braintree.js
@@ -1,16 +1,22 @@
-const express = require('express');
+const express = require("express");
 
 // invoke Express router
 const router = express.Router();
 
-const { requireSignin, isAuth } = require('../controllers/auth');
-const { userById } = require('../controllers/user');
-const { generateToken } = require('../controllers/braintree');
+const { requireSignin, isAuth } = require("../controllers/auth");
+const { userById } = require("../controllers/user");
+const { generateToken, processPayment } = require("../controllers/braintree");
 
+// middleware
+// anytime there is a userId param in the route execute userById method
+router.param("userId", userById);
 
-router.get('/braintree/getToken/:userId', requireSignin, isAuth, generateToken)
-
-
-router.param('userId', userById);
+router.get("/braintree/getToken/:userId", requireSignin, isAuth, generateToken);
+router.post(
+  "/braintree/payment/:userId",
+  requireSignin,
+  isAuth,
+  processPayment
+);
 
 module.exports = router;
