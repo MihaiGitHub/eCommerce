56b775da002c5803a3f4e6c5ac49879b8fc5f7ed
Mihai Smarandache <mihai.sanfran@gmail.com>
2021-01-23 20:40:53 -0700

    Create routes to process order

--- api/app.js
+++ api/app.js
@@ -1,48 +1,51 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 // for dev purpose to view requests in console
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const expressValidator = require('express-validator');
-require('dotenv').config();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const expressValidator = require("express-validator");
+require("dotenv").config();
 
 // import routes
-const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/user');
-const categoryRoutes = require('./routes/category');
-const productRoutes = require('./routes/product');
-const braintreeRoutes = require('./routes/braintree');
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const braintreeRoutes = require("./routes/braintree");
+const orderRoutes = require("./routes/order");
 
 // app
 const app = express();
 
 // db
-mongoose.connect(process.env.DATABASE, {
+mongoose
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
-}).then(() => console.log('DB Connected'));
+    useFindAndModify: false,
+  })
+  .then(() => console.log("DB Connected"));
 
 // middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json()); // get json data from request body
 app.use(cookieParser()); // store user data in cookie
 app.use(expressValidator());
 app.use(cors()); // allow api to handle requests coming from different origins
 
 // use routes as middleware
-app.use('/api', authRoutes);
-app.use('/api', userRoutes);
-app.use('/api', categoryRoutes);
-app.use('/api', productRoutes);
-app.use('/api', braintreeRoutes);
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", braintreeRoutes);
+app.use("/api", orderRoutes);
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.log(`CORS-enabled web server listening on port ${port}`)
+  console.log(`CORS-enabled web server listening on port ${port}`);
 });
-
---
+++ api/routes/order.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+// invoke Express router
+const router = express.Router();
+
+const { requireSignin, isAuth } = require("../controllers/auth");
+const { userById } = require("../controllers/user");
+const { create } = require("../controllers/order");
+
+// middleware
+// anytime there is a userId param in the route execute userById method
+router.param("userId", userById);
+
+// routes
+router.post("/order/create/userId", requireSignin, isAuth, create);
+
+module.exports = router;
