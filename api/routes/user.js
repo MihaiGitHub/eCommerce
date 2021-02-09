14af4351cc13bee156645240becfd381f0cea523
Mihai Smarandache <mihai.sanfran@gmail.com>
2021-02-09 11:28:26 -0700

    Update user profile

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,22 +1,22 @@
-const express = require('express');
+const express = require("express");
 
 // invoke Express router
 const router = express.Router();
-const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
-const { userById, read, update } = require('../controllers/user');
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
+const { userById, read, update } = require("../controllers/user");
 
 // middleware
 // anytime there is a userId param in the route execute userById method
-router.param('userId', userById);
+router.param("userId", userById);
 
 // routes
 // to access this route you have to be currently logged in, currently authenticated user and an admin
-router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
-    res.json({
-        user: req.profile // req.profile filled in middleware userById
-    });
+router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
+  res.json({
+    user: req.profile, // req.profile filled in middleware userById
+  });
 });
-router.get('/user/:userId', requireSignin, isAuth, read);
-router.patch('/user/:userId', requireSignin, isAuth, update);
+router.get("/user/:userId", requireSignin, isAuth, read);
+router.put("/user/:userId", requireSignin, isAuth, update);
 
 module.exports = router;
